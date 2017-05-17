ca1d2942c5477ae6690d0623f802fe69d2f8f43b
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-16 21:40:00 -0700

    Update UserHomepage.php

--- UserHomepage.php
+++ UserHomepage.php
@@ -5,10 +5,8 @@ function UserHomepageBodyCallback(){
 <div class="container">
   <div class="row no-gutters">
     <div class="col-md-12">
-      <h4>Welcome to Many Weiners</h4>
-      <p>Check us out on <a href="https://github.com/cjtrowbridge/manyweiners" target="_blank">Github</a> and please <a href="mailto:chris.j.trowbridge@gmail.com">send me an email</a> if you have any comments of suggestions about how to imrpove this tool!</p>
-    
-      <p>Show nearby guys here and allow users to query other locations.</p>
+      <h4>Welcome to API for REST</h4>
+      <p>Check us out on <a href="https://github.com/cjtrowbridge/apiforrest.com" target="_blank">Github</a> and please <a href="mailto:chris.j.trowbridge@gmail.com">send me an email</a> if you have any comments of suggestions about how to improve this tool!</p>
       
       
     </div>
