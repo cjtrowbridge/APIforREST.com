24fee2fe7d2f0159f55408b8687f1d47c1d4c3a7
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-16 21:38:26 -0700

    Update PublicHomepage.php

--- PublicHomepage.php
+++ PublicHomepage.php
@@ -6,8 +6,9 @@ function PublicHomepageBodyCallback(){
 <div class="container">
 	<div class="row">
 		<div class="col-xs-12">
-			<h1>Welcome to Many Weiners</h1>
-			<p><i>This site lets you see Grindr users who are nearby, as well as some of their past photos and profile text.</i></p>
+			<h1>Welcome to API for REST</h1>
+			<p><i>This site lets you view and explore free public APIs and use them for data science.</i></p>
+			
     </div>
   </div>
 </div>
