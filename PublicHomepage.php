4429eb2155fafd52b19cadf4b4deefbb0ec4e658
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-16 21:39:15 -0700

    Update PublicHomepage.php

--- PublicHomepage.php
+++ PublicHomepage.php
@@ -4,10 +4,10 @@ function PublicHomepageBodyCallback(){
   ?>
 	
 <div class="container">
-	<div class="row">
-		<div class="col-xs-12">
-			<h1>Welcome to API for REST</h1>
-			<p><i>This site lets you view and explore free public APIs and use them for data science.</i></p>
+  <div class="row">
+    <div class="col-xs-12">
+      <h1>Welcome to API for REST</h1>
+      <p><i>This site lets you view and explore free public APIs and use them for data science.</i></p>
 			
     </div>
   </div>
