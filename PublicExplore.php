f38dc6ecaec813b2370764ebba5ab24e5946e787
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-16 21:37:39 -0700

    Update PublicExplore.php

--- PublicExplore.php
+++ PublicExplore.php
@@ -6,10 +6,9 @@ function PublicExploreBodyCallback(){
 <div class="container">
   <div class="row no-gutters">
     <div class="col-md-12">
-      <h4>Explore Many Weiners</h4>
-      <p>Check us out on <a href="https://github.com/cjtrowbridge/manyweiners" target="_blank">Github</a> and please <a href="mailto:chris.j.trowbridge@gmail.com">send me an email</a> if you have any comments of suggestions about how to improve this tool!</p>
+      <h4>Explore API for REST</h4>
+      <p>Check us out on <a href="https://github.com/cjtrowbridge/apiforrest" target="_blank">Github</a> and please <a href="mailto:chris.j.trowbridge@gmail.com">send me an email</a> if you have any comments of suggestions about how to improve this tool!</p>
     
-      <p>List popular guys and maybe a heatmap of user activity and popular search destinations or guy concentrations.</p>
       
       
     </div>
