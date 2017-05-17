e10995611fe036ead5856258681dfe394282dd23
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-16 21:40:29 -0700

    Update PublicExplore.php

--- PublicExplore.php
+++ PublicExplore.php
@@ -7,7 +7,7 @@ function PublicExploreBodyCallback(){
   <div class="row no-gutters">
     <div class="col-md-12">
       <h4>Explore API for REST</h4>
-      <p>Check us out on <a href="https://github.com/cjtrowbridge/apiforrest" target="_blank">Github</a> and please <a href="mailto:chris.j.trowbridge@gmail.com">send me an email</a> if you have any comments of suggestions about how to improve this tool!</p>
+      <p>Check us out on <a href="https://github.com/cjtrowbridge/apiforrest.com" target="_blank">Github</a> and please <a href="mailto:chris.j.trowbridge@gmail.com">send me an email</a> if you have any comments of suggestions about how to improve this tool!</p>
     
       
       
