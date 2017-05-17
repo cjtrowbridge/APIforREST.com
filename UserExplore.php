3fee81c7039b5425e4ae1cc93d77c4bbbe9f0ec5
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-16 21:38:53 -0700

    Update UserExplore.php

--- UserExplore.php
+++ UserExplore.php
@@ -6,10 +6,10 @@ function UserExploreBodyCallback(){
 <div class="container">
   <div class="row no-gutters">
     <div class="col-md-12">
-      <h4>Explore Many Weiners</h4>
-      <p>Check us out on <a href="https://github.com/cjtrowbridge/manyweiners" target="_blank">Github</a> and please <a href="mailto:chris.j.trowbridge@gmail.com">send me an email</a> if you have any comments of suggestions about how to improve this tool!</p>
+      <h4>Explore API for REST</h4>
+      <p>Check us out on <a href="https://github.com/cjtrowbridge/apiforrest.com" target="_blank">Github</a> and please <a href="mailto:chris.j.trowbridge@gmail.com">send me an email</a> if you have any comments of suggestions about how to improve this tool!</p>
     
-      <p>List people I folow and suggest new people to follow.</p>
+      <p>List things I folow and suggest new things to follow.</p>
       <p>Also show all the stuff from the public explore page</p>
       
       
